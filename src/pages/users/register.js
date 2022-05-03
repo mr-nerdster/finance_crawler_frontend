b16668ec9a346ec33aadc67fcaa2f5b2e2834380
NightmareNight-em <nigamharshit1712@gmail.com>
2022-05-03 20:08:59 +0530

    updates

--- src/pages/users/register.js
+++ src/pages/users/register.js
@@ -98,7 +98,7 @@ export default function Register() {
   React.useEffect(() => {
     if (isRegistered) {
       navigate("/login");
-    } else if (userAppErr) formik.resetForm();
+    }
   });
 
   return (
